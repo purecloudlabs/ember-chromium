2d79d216af99f0d7535b597b5fdf1c064e2c4a87
Xander Dumaine <xander.dumaine@genesys.com>
2018-12-20 15:51:02 -0500

    Refactor to extract to /ember-chromium/<version>/

--- index.js
+++ index.js
@@ -17,32 +17,33 @@ function getTestemConfig (chromiumArgs) {
     chromiumArgs = defaultChromiumArgs;
   }
 
-  const {execPath} = utils.getBinaryPath();
-  if (!fs.existsSync(execPath)) {
-    console.error('Chromium does not appear to be installed, testem cannot run.'); // eslint-disable-line
-    process.exit(1); // for some reason this doesn't stop testem from continuing in server mode
-    throw new Error('Chromium not installed');
-  }
+  return utils.getBinaryPath().then(({execPath}) => {
+    if (!fs.existsSync(execPath)) {
+      console.error('Chromium does not appear to be installed, testem cannot run.'); // eslint-disable-line
+      process.exit(1); // for some reason this doesn't stop testem from continuing in server mode
+      throw new Error('Chromium not installed');
+    }
 
-  return {
-    test_page: 'tests/index.html?hidepassed',
-    browser_start_timeout: 60,
-    launchers: {
-      chromium: {
-        exe: execPath,
-        args: chromiumArgs,
-        protocol: 'browser'
+    return {
+      test_page: 'tests/index.html?hidepassed',
+      browser_start_timeout: 60,
+      launchers: {
+        chromium: {
+          exe: execPath,
+          args: chromiumArgs,
+          protocol: 'browser'
+        },
+        chromium_headless: {
+          exe: execPath,
+          args: ['--headless', ...chromiumArgs],
+          protocol: 'browser'
+        }
       },
-      chromium_headless: {
-        exe: execPath,
-        args: ['--headless', ...chromiumArgs],
-        protocol: 'browser'
-      }
-    },
 
-    launch_in_ci: ['chromium_headless'],
-    launch_in_dev: ['chromium']
-  };
+      launch_in_ci: ['chromium_headless'],
+      launch_in_dev: ['chromium']
+    };
+  });
 }
 
 module.exports = {
--- utils.js
+++ utils.js
@@ -3,32 +3,116 @@
 'use strict';
 
 const path = require('path');
+const got = require('got');
 const childProcess = require('child_process');
 
-function getBinaryPath () {
-  const buffer = childProcess.execSync('npm bin -g');
-  const result = String.fromCharCode.apply(null, buffer);
-  const globalPath = result.replace(/\n$/, '');
-  let binPath = globalPath;
-  let execPath;
-
+function getCurrentOs () {
   const platform = process.platform;
-  const folderName = getOsChromiumFolderName();
 
   if (platform === 'linux') {
-    execPath = path.join(binPath, folderName, 'chrome');
-  } else if (platform === 'win32') {
-    execPath = path.join(binPath, folderName, 'chrome.exe');
-  } else if (platform === 'darwin') {
-    execPath = path.join(binPath, folderName, 'Chromium.app/Contents/MacOS/Chromium');
-  } else {
-    console.error('Unsupported platform or architecture found:', process.platform, process.arch);
-    throw new Error('Unsupported platform');
+    return 'linux';
   }
 
-  console.log(`checking for chromium at: ${execPath.toString()}`);
+  if (platform === 'win32') {
+    return 'win';
+  }
 
-  return {binPath, execPath};
+  if (platform === 'darwin') {
+    return 'mac';
+  }
+
+  console.log('Unknown platform found:', process.platform);
+  throw new Error('Unsupported platform');
+}
+
+const version = process.env.CHROMIUM_VERSION || '67.0.0';
+const versionsWithUnknownBranchingPoint = [];
+function getExactChromeVersionNumber () {
+  return new Promise((resolve, reject) => {
+    const url = 'https://omahaproxy.appspot.com/history.json?channel=' + (process.env.CHROMIUM_CHANNEL || 'dev') + '&os=' + getCurrentOs();
+    const packageMajorVersion = version.split('.')[0];
+
+    got(url)
+      .then(response => {
+        let versions = JSON.parse(response.body);
+
+        for (let version of versions) {
+          let versionNumber = version.version;
+          let buildMajorVersion = versionNumber.split('.')[0];
+
+          if (buildMajorVersion !== packageMajorVersion) {
+            continue;
+          }
+
+          if (versionsWithUnknownBranchingPoint.includes(versionNumber)) {
+            continue;
+          }
+
+          return resolve(versionNumber);
+        }
+      }
+      )
+      .catch(err => {
+        console.log('An error occured while trying to retrieve latest revision number', err);
+        reject(err);
+      });
+  });
+}
+
+function getBinaryPath () {
+  return getExactChromeVersionNumber()
+    .then(versionNumber => {
+      const buffer = childProcess.execSync('npm bin -g');
+      const result = String.fromCharCode.apply(null, buffer);
+      const globalPath = result.replace(/\n$/, '');
+      let binPath = path.join(globalPath, 'ember-chromium', versionNumber);
+      let execPath;
+
+      const platform = process.platform;
+      const folderName = getOsChromiumFolderName();
+
+      if (platform === 'linux') {
+        execPath = path.join(binPath, folderName, 'chrome');
+      } else if (platform === 'win32') {
+        execPath = path.join(binPath, folderName, 'chrome.exe');
+      } else if (platform === 'darwin') {
+        execPath = path.join(binPath, folderName, 'Chromium.app/Contents/MacOS/Chromium');
+      } else {
+        console.error('Unsupported platform or architecture found:', process.platform, process.arch);
+        throw new Error('Unsupported platform');
+      }
+
+      console.log(`checking for chromium at: ${execPath.toString()}`);
+
+      return {binPath, execPath, versionNumber};
+    });
+}
+
+function getChromiumBranchingPoint (versionNumber) {
+  return new Promise((resolve, reject) => {
+    const url = 'https://omahaproxy.appspot.com/deps.json?version=' + versionNumber;
+
+    got(url)
+      .then(response => {
+        let versionDetails = JSON.parse(response.body);
+        let branchingPoint = parseInt(versionDetails.chromium_base_position);
+
+        if (!Number.isInteger(branchingPoint)) {
+          console.log('Could not find branching point for Chrome ' + versionNumber + '. This can happen when the new Chrome version is just branched off. Let\'s try to find the branching point for one version earlier.');
+          versionsWithUnknownBranchingPoint.push(versionNumber);
+
+          resolve(getExactChromeVersionNumber().then(getChromiumBranchingPoint));
+        }
+
+        console.log('Found that Chrome ' + versionNumber + ' was branched off Chromium at point ' + branchingPoint);
+
+        resolve(branchingPoint);
+      })
+      .catch(err => {
+        console.log('Could not get build details for version ' + versionNumber);
+        reject(err);
+      });
+  });
 }
 
 function getOsChromiumFolderName () {
@@ -45,5 +129,6 @@ function getOsChromiumFolderName () {
 
 module.exports = {
   getBinaryPath,
-  getOsChromiumFolderName
+  getOsChromiumFolderName,
+  getChromiumBranchingPoint
 };
